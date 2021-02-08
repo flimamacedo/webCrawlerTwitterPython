92ef77fae0eef49983f66742dd04949a75e761db
Fernando Macedo <macedo.fernando@outlook.com>
2021-02-08 09:04:27 -0300

    adjusts

--- sarg/app/sarg/settings.py
+++ sarg/app/sarg/settings.py
@@ -26,7 +26,7 @@ chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 SECRET_KEY = get_random_string(50, chars)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'caseitau.fastsolutionsoncloud.com.br', 'web', '127.0.0.1']
 
